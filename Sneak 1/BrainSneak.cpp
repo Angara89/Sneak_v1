d911fe25da0b1ee4fc4dc8fc04d2a3d45d2503f3
svain <svainform@gmail.com>
2023-01-30 14:41:12 +0300

    начало разработки логики змейки с постройки пути

--- Sneak 1/BrainSneak.cpp
+++ Sneak 1/BrainSneak.cpp
@@ -139,7 +139,6 @@ void BrainSneak::usersDecide(int k)
 	}
 }
 	
-
 bool BrainSneak::compareCoordApple(COORD apple)
 {
 	return ((apple.X != arrX[length - 1]) && (apple.Y != arrY[length - 1]));
@@ -173,3 +172,113 @@ COORD BrainSneak::getTail()
 	return Tail;
 }
 
+void BrainSneak::decide2(COORD apple)
+{
+	COORD head = getHead();
+
+	char lastChoose;// узнаем какой был последний выбор
+	if (dy == 1)
+		lastChoose = 'd';
+	if (dx == 1)
+		lastChoose = 'r';
+	if (dy == -1)
+		lastChoose = 'u';
+	if (dx == -1)
+		lastChoose = 'l';
+
+	char choose;
+	if (apple.X == head.X && apple.Y == head.Y)
+		choose = 'n';
+
+	if (apple.X == head.X && apple.Y > head.Y)
+		choose = 'd';
+	if (apple.X == head.X && apple.Y < head.Y)
+		choose = 'u';
+
+	if (apple.Y == head.Y && apple.X > head.X)
+		choose = 'r';
+	if (apple.Y == head.Y && apple.X < head.X)
+		choose = 'l';
+
+	if (apple.Y > head.Y)
+		choose = 'd';
+	if (apple.Y < head.Y)
+		choose = 'u';
+
+	bool rightChoose = true;
+	int tdy;
+	int tdx;
+	int count = 0;
+
+	do
+	{
+		if (rightChoose == false && count == 1)
+			choose = 'd';
+		if (rightChoose == false && count == 2)
+			choose = 'u';
+		if (rightChoose == false && count == 3)
+			choose = 'r';
+		if (rightChoose == false && count == 4)
+			choose = 'l';
+		if (rightChoose == false && count == 5)
+			choose = 'n';
+
+		switch (choose) // пропускаем код нажатой клавиши внутрь оператора выбора
+		{
+		case 'd': // если была нажата клавиша вниз
+			tdy = 1; // то приращение по оси ординат будет положительным
+			tdx = 0; // по оси абсцисс приращение нулевое
+			break;
+		case 'u': // если вверх
+			tdy = -1; // аналогично согласно геометрической логике
+			tdx = 0;
+			break;
+		case 'l': // если влево
+			tdy = 0;
+			tdx = -1;
+			break;
+		case 'r': // если вправо
+			tdy = 0;
+			tdx = 1;
+			break;
+		case 'n': // если ничего
+			tdy = dy;
+			tdx = dx;
+			break;
+		}
+		rightChoose = true;
+		for (int i = 1; i < length; i++)
+		{
+			int r = arrX.back() + tdx;
+			int rr = arrX[i];
+			if ((r == rr) && ((arrY.back() + tdy) == arrY[i])) // если совпадение найдено в цикле - прерываемся
+				rightChoose = false;
+		}
+		if (count == 100)
+			throw BrainSneakEx();
+		++count;
+
+	} while (!rightChoose);
+
+	switch (choose) // пропускаем код нажатой клавиши внутрь оператора выбора
+	{
+	case 'd': // если была нажата клавиша вниз
+		dy = 1; // то приращение по оси ординат будет положительным
+		dx = 0; // по оси абсцисс приращение нулевое
+		break;
+	case 'u': // если вверх
+		dy = -1; // аналогично согласно геометрической логике
+		dx = 0;
+		break;
+	case 'l': // если влево
+		dy = 0;
+		dx = -1;
+		break;
+	case 'r': // если вправо
+		dy = 0;
+		dx = 1;
+		break;
+	case 'n': // если ничего
+		break;
+	}
+}
--- Sneak 1/BrainSneak.h
+++ Sneak 1/BrainSneak.h
@@ -1,7 +1,7 @@
 #pragma once 
 #include <Windows.h>
 #include <vector>
-
+#include <queue>
 const int max_length = 500;
 class BrainSneak
 {
@@ -16,7 +16,11 @@ public:
 	std::vector<int> arrX;
 	std::vector<int> arrY;
 	int length;
+	std::queue<int> path;
+
+
 	void decide(COORD apple);
+	void decide2(COORD apple);
 	void usersDecide(int k);
 	bool compareCoordApple(COORD apple);
 	COORD getHead();
